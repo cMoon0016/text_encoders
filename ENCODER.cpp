f837388eae666a10d689711ec8d038b00cbbdd5a
cMoon0016 <82235157+cMoon0016@users.noreply.github.com>
2021-04-10 17:15:05 +0200

    Update ENCODER.cpp

--- ENCODER.cpp
+++ ENCODER.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <algorithm>
 
-std::string encode(std::string text, int encoder = 1) //could add more encoders, for e.g. encoder of Morse code
+std::string encode(std::string text, int encoder = 1) 
 {
-    std::transform(text.begin(), text.end(), text.begin(), ::toupper);
+    std::transform(text.begin(), text.end(), text.begin(), ::toupper); //transform all letters to capital ones
     std::string encoded = "";
 
-    if(encoder == 1)
+    switch(encoder)
     {
-        for(int i=0; i<text.length(); i++)
-            encoded += encodeLetter(text[i]); //transmit each char to encoder function
+        case 1:
+        {
+            for(int i=0; i<text.length(); i++)
+                encoded += encodeLetter(text[i]); //transmit each char to numeric encoder function
+            break;
+        }
+        case 2:
+        {
+            for(int i=0; i<text.length(); i++)
+                encoded += encodeMorse(text[i]); //transmit each char to Morse encoder function
+            break;
+        }
+        //here could add more encoders
+        default:
+            return "Wrong encoder number";
+
     }
-    //here could add more encoders
 
     return encoded;
 }
@@ -23,7 +36,8 @@ int main()
     std::string text;
     getline(std::cin, text);
 
-    std::cout<<text<<" = "<<encode(text);
+    std::cout<<text<<" = "<<encode(text)<<std::endl; //numeric encode
+    std::cout<<text<<" = "<<encode(text, 2)<<std::endl; //Morse encode
     return 0;
 }
 
